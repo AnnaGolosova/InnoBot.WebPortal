51f7515198eeb8d42b1e2656df411f811c9e32ba
golosowa.anna <golosowa.anna@gmail.com>
2021-12-21 23:44:44 +0300

    Initial commit with base SignalR implementation

---
+++ InnoBot.WebPortal/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using InnoBot.WebPortal.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace InnoBot.WebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ReceiveMessage(string message)
+        {
+            return View("Index");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ InnoBot.WebPortal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace InnoBot.WebPortal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
